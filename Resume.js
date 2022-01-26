2f6325fffd11e3416d368163276c9341080d2c88
c-marks13 <92548158+c-marks13@users.noreply.github.com>
2022-01-26 15:43:21 -0600

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,19 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  //Get the button
+  var toTop = document.getElementById("ToTopBTN");
+
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
