48daeab3eaf0cfd07e9cb2b7def14e781e066fd8
c-marks13 <connor.marks6@gmail.com>
2022-01-30 17:56:04 -0600

    Finished nav menu stuff

--- Resume.js
+++ Resume.js
@@ -3,20 +3,33 @@ window.onload = function() {navMenu()};
 function navMenu() {
   //Get the button
   var nm = document.getElementById("Nav-Menu");
+
   var x = document.getElementById("DropDown");
   x.style.display = "none";
+
   nm.addEventListener("click", openMenu);
+
+  var ddContent = document.getElementsByClassName("dd-content");
+  for(let i=0; i<ddContent.length; i++){
+    ddContent[i].addEventListener("click", closeMenu);
+  }
+
+  window.onscroll = closeMenu;
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function openMenu() {
-  console.log(1);
   var x = document.getElementById("DropDown");
 
   if(x.style.display == "none"){
     x.style.display = "block";
   }
-  else {
+  else{
     x.style.display = "none";
   }
+}
+
+function closeMenu(){
+  var x = document.getElementById("DropDown");
+  x.style.display = "none";
 }
